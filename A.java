4979a3de86e0700ad4ab9a793fbe22933d2f06c5
yong <ckddyd6587@naver.com>
2025-03-07 15:15:38 +0900

    ㅎㅎㅎㅎㅎ해치웠나..?

--- A.java
+++ A.java
@@ -1,7 +1,7 @@
 public class A {
     int a = 77;
     String str = "나는 진짜 바보입니다... 죄송합니다"
-    String str = "전창용짱이다친구많음."
+    String str = "전창용짱이다친구많음이거는 진짜임 ㄹㅇ."
     int age = 51;
     double b = 77.7;
 }
